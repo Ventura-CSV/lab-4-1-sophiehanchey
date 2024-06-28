0976eaf630da526f0315d5e1df28a82c3f01ed46
Sophie Hanchey <sophiehanchey@gmail.com>
2024-06-28 11:13:22 -0700

    initial code, passed pytest

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ def main():
     N = int(input('Enter the number N: '))
     result = []
 
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    for i in range(N+1):
+        new_num = 2**i
+        result.append(new_num)
+        
+    print(result)
 
     ########################################
     # Do not delete the return statement
